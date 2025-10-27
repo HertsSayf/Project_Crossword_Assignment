8837b55f3ac5bceecce41ad213cce48516fb120f
Fayaaz <148206110+Piaj7@users.noreply.github.com>
2025-10-27 15:06:16 +0000

    trying to get the basics of the game

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ grid = [
     ['', '', '', '', ''],
     ['', '', '', '', '']
 ]
+
+def display_grid():
+    for row in grid:
+        print(' | '.join(cell if cell != '' else ' ' for cell in row))
+        print('-' * 21)
+
+display_grid()
+
+word_list = ['PYTHON', 'JAVA', 'RUBY', 'SWIFT', 'KOTLIN']
+
