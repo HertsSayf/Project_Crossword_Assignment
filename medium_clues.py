1aa169dd0f099c38902a7563c46da0724097cd88
RayRay06 <rayyankool353@gmail.com>
2025-10-28 22:11:58 +0000

    addded medium clues

--- medium_clues.py
+++ medium_clues.py
@@ -1,1 +1,7 @@
-
+medium_clues = {
+    'VARIABLE': 'A named storage location in a program that holds a value that can change.',
+    'FUNCTION': 'A reusable block of code that performs a specific task when called.',
+    'SYNTAX': 'The set of rules that defines how code must be written so the computer understands it.',
+    'BOOLEAN': 'A type of value that can only be true or false.',
+    'LOOP': 'A way to repeat a block of code several times until a condition is met.'
+}
