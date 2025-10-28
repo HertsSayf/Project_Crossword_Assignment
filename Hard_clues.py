42d5e0f610b1f96368307cb1dc319145d3720a95
RayRay06 <rayyankool353@gmail.com>
2025-10-28 22:00:30 +0000

    added harder clues for advanced crossword

--- Hard_clues.py
+++ Hard_clues.py
@@ -1,1 +1,7 @@
-
+Hard_clues = {
+    'ALGORITHM': 'A logical set of steps used to solve a problem or complete a task, kind of like a recipe for computers.',
+    'ENCRYPTION': 'A way of scrambling information so only people with the right key can read it.',
+    'COMPILE': 'What happens when your code gets turned into something the computer can actually run.',
+    'RECURSION': 'When a function calls itself to solve a problem, it is code looping in on itself.',
+    'DEBUGGING': 'The not so fun but very important process of finding and fixing mistakes in your code.'
+}
